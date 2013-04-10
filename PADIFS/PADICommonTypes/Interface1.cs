16d3f5a124b184517695d51310fc822a9e1d0cc2
BennSaturn <bernardo.mpsantos@gmail.com>
2013-04-10 12:28:58 +0100

    MDServer methods inserted as well as CommonTypes.

--- PADIFS/PADICommonTypes/Interface1.cs
+++ PADIFS/PADICommonTypes/Interface1.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace PADICommonTypes
 {
+
+    public delegate void DebugDelegate(string mensagem);
+    
     interface IMDServer
     {
+        void OPEN(string filename);
+        void CLOSE(string filename);
+        void CREATE(string filename, int nb_dataservers, int read_quorum, int write_quorum);
+        void DELETE(string filename);
+        void RECOVER(string mdserver);
+        void FAIL(string mdserver);
+    }
+
+    interface IDServer
+    {
+        void READ(string filename, string semantics);
+        void WRITE(string filename, byte[] content);
+        void FREEZE(string dserver);
+        void UNFREEZE(string dserver);
+        void FAIL(string dserver);
+        void RECOVER(string dserver);
+    }
+
+    interface IClient 
+    {
+        void CREATE(string clientname, string filename);
+        void OPEN(string clientname, string filename);
+        void CLOSE(string clientname, string filename);
+        void READ(string clientname, string filename, string semantics);
+        void WRITE(string clientname, string filename, byte[] content); 
+        void DELETE(string clientname, string filename);
     }
 }
