d304e3d97b96895368476a43d81e0689c1eb2040
Luís Carvalho <l.rccarvalho@gmail.com>
2013-04-10 12:31:20 +0100

    read commands

--- PADIFS/PuppetMaster/PuppetMasterForm.Designer.cs
+++ PADIFS/PuppetMaster/PuppetMasterForm.Designer.cs
@@ -28,58 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loadbt = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.stepbt = new System.Windows.Forms.Button();
-            this.runbt = new System.Windows.Forms.Button();
+            this.loadBT = new System.Windows.Forms.Button();
+            this.scriptPathTX = new System.Windows.Forms.TextBox();
+            this.stepBT = new System.Windows.Forms.Button();
+            this.runBT = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // loadbt
             // 
-            this.loadbt.Location = new System.Drawing.Point(12, 38);
-            this.loadbt.Name = "loadbt";
-            this.loadbt.Size = new System.Drawing.Size(75, 23);
-            this.loadbt.TabIndex = 0;
-            this.loadbt.Text = "Load Script";
-            this.loadbt.UseVisualStyleBackColor = true;
-            this.loadbt.Click += new System.EventHandler(this.loadbt_Click);
+            this.loadBT.Location = new System.Drawing.Point(12, 38);
+            this.loadBT.Name = "loadbt";
+            this.loadBT.Size = new System.Drawing.Size(75, 23);
+            this.loadBT.TabIndex = 0;
+            this.loadBT.Text = "Load Script";
+            this.loadBT.UseVisualStyleBackColor = true;
+            this.loadBT.Click += new System.EventHandler(this.loadbt_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(237, 20);
-            this.textBox1.TabIndex = 1;
+            this.scriptPathTX.Location = new System.Drawing.Point(12, 12);
+            this.scriptPathTX.Name = "textBox1";
+            this.scriptPathTX.Size = new System.Drawing.Size(237, 20);
+            this.scriptPathTX.TabIndex = 1;
             // 
             // stepbt
             // 
-            this.stepbt.Location = new System.Drawing.Point(174, 38);
-            this.stepbt.Name = "stepbt";
-            this.stepbt.Size = new System.Drawing.Size(75, 23);
-            this.stepbt.TabIndex = 2;
-            this.stepbt.Text = "Next Step";
-            this.stepbt.UseVisualStyleBackColor = true;
-            this.stepbt.Click += new System.EventHandler(this.stepbt_Click);
+            this.stepBT.Location = new System.Drawing.Point(174, 38);
+            this.stepBT.Name = "stepbt";
+            this.stepBT.Size = new System.Drawing.Size(75, 23);
+            this.stepBT.TabIndex = 2;
+            this.stepBT.Text = "Next Step";
+            this.stepBT.UseVisualStyleBackColor = true;
+            this.stepBT.Click += new System.EventHandler(this.stepbt_Click);
             // 
             // runbt
             // 
-            this.runbt.Location = new System.Drawing.Point(93, 38);
-            this.runbt.Name = "runbt";
-            this.runbt.Size = new System.Drawing.Size(75, 23);
-            this.runbt.TabIndex = 3;
-            this.runbt.Text = "Run";
-            this.runbt.UseVisualStyleBackColor = true;
-            this.runbt.Click += new System.EventHandler(this.runbt_Click);
+            this.runBT.Location = new System.Drawing.Point(93, 38);
+            this.runBT.Name = "runbt";
+            this.runBT.Size = new System.Drawing.Size(75, 23);
+            this.runBT.TabIndex = 3;
+            this.runBT.Text = "Run";
+            this.runBT.UseVisualStyleBackColor = true;
+            this.runBT.Click += new System.EventHandler(this.runbt_Click);
             // 
             // PuppetMasterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(261, 142);
-            this.Controls.Add(this.runbt);
-            this.Controls.Add(this.stepbt);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.loadbt);
+            this.Controls.Add(this.runBT);
+            this.Controls.Add(this.stepBT);
+            this.Controls.Add(this.scriptPathTX);
+            this.Controls.Add(this.loadBT);
             this.Name = "PuppetMasterForm";
             this.Text = "PuppetMaster";
             this.Load += new System.EventHandler(this.PuppetMasterForm_Load);
@@ -90,9 +90,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button loadbt;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button stepbt;
-        private System.Windows.Forms.Button runbt;
+        private System.Windows.Forms.Button loadBT;
+        private System.Windows.Forms.TextBox scriptPathTX;
+        private System.Windows.Forms.Button stepBT;
+        private System.Windows.Forms.Button runBT;
     }
 }
