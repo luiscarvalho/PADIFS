b2adbf210c6b60a5169e38440396dadca033c1c9
RaquelB <raquel.bairrao@ist.utl.pt>
2013-04-11 13:25:47 +0100

    Clientmethodsadded

--- PADIFS/Client/Client.cs
+++ PADIFS/Client/Client.cs
@@ -6,39 +6,68 @@ using System.Runtime.Remoting.Channels;
 using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
 using System.Threading.Tasks;
+using PADICommonTypes;
 
 namespace Client
 {
     class Client
     {
+        static int nclient = 0;
+        static string clientname = "c-";
+
         static void Main(string[] args)
         {
             TcpChannel channel = new TcpChannel();
             ChannelServices.RegisterChannel(channel, true);
             RemotingConfiguration.RegisterWellKnownServiceType(typeof(Client), "ClientRemote",
             WellKnownObjectMode.Singleton);
-            //Server s = (Server)Activator.GetObject(typeof(Server)
-             //   , "tcp://localhost:" + 8086 + "/ClienteRemote")        
+            clientname += nclient.ToString();
+            System.Console.WriteLine("Client c-" + clientname + " on");
+            nclient++;
+            System.Console.ReadLine();
         }
 
-        public class Client : MarshalByRefObject //, IClient
+        public class Client : MarshalByRefObject, IClient, IMDServer, IDServer
         {
 
 
-            public void create(string nome, string msg)
+            public void CREATE(string clientname, string filename, int nb_dataservers,
+                int read_quorum, int write_quorum, DebugDelegate debug)
             {
+                IMDServer mdscreate = (IMDServer)Activator.GetObject(typeof(IMDServer)
+                , "tcp://localhost:8086/MetaData_Server");
+                mdscreate.CREATE(filename, nb_dataservers, read_quorum, write_quorum);
             }
-
-            public void open(string nome, string msg)
+            public void OPEN(string clientname, string filename, DebugDelegate debug)
             {
+                IMDServer mdsopen = (IMDServer)Activator.GetObject(typeof(IMDServer)
+                , "tcp://localhost:8086/MetaData_Server");
+                mdsopen.OPEN(filename);
             }
-
-            public void close(string nome, string msg)
+            public void CLOSE(string clientname, string filename, DebugDelegate debug)
             {
+                IMDServer mdsclose = (IMDServer)Activator.GetObject(typeof(IMDServer)
+                , "tcp://localhost:8086/MetaData_Server");
+                mdsclose.CLOSE(filename);
             }
-
-            public void delete(string nome, string msg)
+            public void READ(string clientname, string filename, string semantics, DebugDelegate debug)
             {
+                IDServer dsread = (IDServer)Activator.GetObject(typeof(IDServer)
+                , "tcp://localhost:8087/Data_Server");
+                dsread.READ(filename, semantics);
             }
+            public void WRITE(string clientname, string filename, byte[] content, DebugDelegate debug)
+            {
+                IDServer dswrite = (IDServer)Activator.GetObject(typeof(IDServer)
+                , "tcp://localhost:8087/Data_Server");
+                dswrite.WRITE(filename, content);
+            }
+            public void DELETE(string clientname, string filename, DebugDelegate debug)
+            {
+                IMDServer mdsdelete = (IMDServer)Activator.GetObject(typeof(IMDServer)
+                , "tcp://localhost:8086/MetaData_Server");
+                mdsdelete.DELETE(filename);
+            }
+        }
     }
 }
